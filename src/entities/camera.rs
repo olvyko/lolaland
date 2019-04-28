2bad5a98e1241b9c4566b737de93203d7cfe5c2e
David Ackerman <enfipy@gmail.com>
2019-04-29 01:24:17 +0300

    Alter camera

--- src/entities/camera.rs
+++ src/entities/camera.rs
@@ -12,11 +12,11 @@ pub fn init_camera(world: &mut World) {
    let mut camera_transform = Transform::default();
    camera_transform.set_translation_z(1.0);
 
+   let camera = Camera::from(Projection::orthographic(0.0, width, 0.0, height));
+
    world
       .create_entity()
       .with(camera_transform)
-      .with(Camera::from(Projection::orthographic(
-         0.0, width, 0.0, height,
-      )))
+      .with(camera)
       .build();
 }
