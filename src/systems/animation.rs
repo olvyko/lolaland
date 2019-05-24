2f5ac240c3e8ad6f777b551df5f14cd949060d54
olvyko <olvyko@gmail.com>
2019-05-24 11:39:28 +0200

    Fix animation system

--- src/systems/animation.rs
+++ src/systems/animation.rs
@@ -20,17 +20,42 @@ impl<'s> System<'s> for AnimationSystem {
     );
 
     fn run(&mut self, (entities, animation_sets, mut control_sets, input): Self::SystemData) {
+        // if pos == neg { 0.0 } else if pos { 1.0 } else { -1.0 }
+        let x_move = input.axis_value("x_move").unwrap();
+
         // For each entity that has AnimationSet
         for (entity, animation_set) in (&entities, &animation_sets).join() {
             // Creates a new AnimationControlSet for the entity
             let control_set = get_animation_set(&mut control_sets, entity).unwrap();
-            control_set.add_animation(
-                AnimationId::Idle,
-                &animation_set.get(&AnimationId::Idle).unwrap(),
-                EndControl::Loop(None),
-                1.0,
-                AnimationCommand::Start,
-            );
+
+            if x_move > 0.0 {
+                println!("right");
+                control_set.add_animation(
+                    AnimationId::Walk,
+                    &animation_set.get(&AnimationId::Walk).unwrap(),
+                    EndControl::Loop(None),
+                    1.0,
+                    AnimationCommand::Start,
+                );
+            } else if x_move < 0.0 {
+                println!("left");
+                control_set.add_animation(
+                    AnimationId::Walk,
+                    &animation_set.get(&AnimationId::Walk).unwrap(),
+                    EndControl::Loop(None),
+                    1.0,
+                    AnimationCommand::Start,
+                );
+            } else {
+                println!("idle");
+                control_set.add_animation(
+                    AnimationId::Idle,
+                    &animation_set.get(&AnimationId::Idle).unwrap(),
+                    EndControl::Loop(None),
+                    1.0,
+                    AnimationCommand::Start,
+                );
+            }
         }
     }
 }
--- src/systems/bundle.rs
+++ src/systems/bundle.rs
@@ -9,7 +9,7 @@ impl<'a, 'b> SystemBundle<'a, 'b> for GameBundle {
     fn build(self, builder: &mut DispatcherBuilder<'a, 'b>) -> Result<(), amethyst::Error> {
         builder.add(MovementSystem, "movement_system", &["input_system"]);
         builder.add(
-            MovementSystem,
+            AnimationSystem,
             "animation_system",
             &[
                 "input_system",
